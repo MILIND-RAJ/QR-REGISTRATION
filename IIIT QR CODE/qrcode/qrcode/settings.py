a55b6f64ecff759ae152debafa0ba45921682a01
MILIND-RAJ <48434586+MILIND-RAJ@users.noreply.github.com>
2019-12-05 16:52:33 +0530

    Update settings.py

--- IIIT QR CODE/qrcode/qrcode/settings.py
+++ IIIT QR CODE/qrcode/qrcode/settings.py
@@ -124,6 +124,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'opensources.mr@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mraj@1000'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'Password'
 
